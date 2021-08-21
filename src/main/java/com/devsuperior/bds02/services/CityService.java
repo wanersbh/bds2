357a336b70476aac7094bcf1c9e6dc8a4d41a01b
waner <wanersbh@gmail.com>
2021-08-21 12:01:25 -0300

    Development of city layers.

---
+++ src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -0,0 +1,44 @@
+package com.devsuperior.bds02.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.entities.City;
+import com.devsuperior.bds02.repositories.CityRepository;
+import com.devsuperior.bds02.services.exceptions.DatabaseException;
+import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class CityService {
+	
+	@Autowired
+	private CityRepository repository;
+	
+	public void delete(Long id) {
+		
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Not found id: "+ id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violetion.");
+		}
+		
+	}
+	
+	@Transactional(readOnly = true)
+	public List<CityDTO> findAll(){
+		
+		List<City> result = repository.findAll(Sort.by("name"));
+		
+		return result.stream().map(x ->  new CityDTO(x) ).toList();
+	}
+
+}
